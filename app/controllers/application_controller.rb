dc159af9512fa394e73c54d2329f9cfe622596f1
yadavsudhir405 <yadavsudhir405@gmail.com>
2017-08-01 21:40:11 +0530

    Add modified git ignore files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
   private
     def set_locale
       I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales) ||I18n.default_locale
+
     end
 end
