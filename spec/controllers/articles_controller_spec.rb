9c8f59f6e9dc8674ba9d4ce14a9019bf3574472f
yadavsudhir405 <yadavsudhir405@gmail.com>
2017-08-03 23:13:42 +0530

    Add article controller spec

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails-helper'
 
 Rspec.describe ArticlesController, type: :controller do
-  let(:article) = {}
+  describe "GET #new" do
+    it "respondes successfully with  HTTP status code 200" do
+      get :new
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
 
+  end
 end
