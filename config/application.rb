06be2adb9346d587a6d844938840a4b0b9fc11d1
yadavsudhir405 <yadavsudhir405@gmail.com>
2017-08-04 19:44:12 +0530

    Add sidekiq adpater

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Blog
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.active_job.queue_adapter = :sidekiq
+
     config.i18n.default_locale = :en
     config.i18n.available_locales = [:en, :hi]
     config.i18n.fallbacks = true
