b2e74ebfbfc9ad6faca5004b935af33f98bb4a28
yadavsudhir405 <yadavsudhir405@gmail.com>
2017-08-14 16:26:43 +0530

    Add commit delete code

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 
 class Article < ActiveRecord::Base
 
-  has_many :comments
+  has_many :comments,dependent: :destroy
 
   validates :title, presence: true, :length => { minimum: 5 }
   validates :text,presence: true,length: {:minimum => 5,maximum: 20}
