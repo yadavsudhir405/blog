6f279a68c3751acce5ed8a774f101dbe54223eb4
yadavsudhir405 <yadavsudhir405@gmail.com>
2017-08-14 15:50:25 +0530

    Add comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   resources :trips
   resources :persons
 
