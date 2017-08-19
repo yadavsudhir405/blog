1ee8586642576d8b1bab19d427648e8ad4a5818d
yadavsudhir405 <yadavsudhir405@gmail.com>
2017-08-19 11:25:27 +0530

    Added shared examples conceptes in specs

--- spec/examples/cores/stack_spec.rb
+++ spec/examples/cores/stack_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
+RSpec.shared_examples "some example" do |parameter|
+  let(:parameter) {parameter}
+
+  it "uses the given parameter" do
+    expect(parameter).to eq parameter
+  end
+end
+
+
 RSpec.describe "Using an array as stack" do
 
   def build_stack
     []
   end
-
   before(:example) do
       @stack = build_stack
   end
 
   it "is initaially empty" do
     expect(@stack).to be_empty
+    expect(@stack.length).to eq 0
   end
 
   context  "after an item pushed to stack" do
@@ -26,4 +35,7 @@ RSpec.describe "Using an array as stack" do
     end
   end
 
+  include_examples "some example","parameter1"
+  include_examples "some example","parameter2"
 end
+
