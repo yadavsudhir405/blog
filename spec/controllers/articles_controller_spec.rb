7a6ae8bf712f352c0a690b752f8e67a721c9a337
yadavsudhir405 <yadavsudhir405@gmail.com>
2017-08-21 22:49:39 +0530

    Add spec for controller

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -10,7 +10,18 @@ RSpec.describe ArticlesController, type: :controller do
       @article = Article.new
       get :new
 
+      expect(response).to be_success
       expect(response).to render_template('new')
     end
   end
+  describe "show index page" do
+      it "should open the index page successfully " do
+        get :index
+
+        expect(response).to be_success
+        expect(response).to render_template('index')
+      end
+  end
+
+  desc
 end
