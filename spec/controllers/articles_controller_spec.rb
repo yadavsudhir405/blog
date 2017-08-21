d4f3f9b1cd42fa791c8ae0ea292d59af76fca13f
yadavsudhir405 <yadavsudhir405@gmail.com>
2017-08-21 22:44:46 +0530

    Add controller spec

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
+  include AuthHelper
   describe "#new" do
+    before(:example) do
+      http_login
+    end
     it "should open the new articlpe page " do
       @article = Article.new
       get :new
 
-      expect(response).to be_success
+      expect(response).to render_template('new')
     end
   end
 end
