244bf38344ef2b018fc1ab2064596d64d3452e9b
yadavsudhir405 <yadavsudhir405@gmail.com>
2017-11-01 23:21:03 +0530

    Add

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-require 'sidekiq'
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{Figaro.env.sidekiq_redis_host}" }
-end
-
-Sidekiq.configure_client do |config|
-    config.redis = { url: "redis://#{Figaro.env.sidekiq_redis_host}" }
-end
-
